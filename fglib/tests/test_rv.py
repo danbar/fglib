db30e11a0c0bf6ca2078eebee81ce455c1fb6b53
Daniel Bartel <dan.bar@gmx.at>
2014-01-11 16:47:39 +0100

    Improve unit tests for random variables.

--- fglib/tests/test_rv.py
+++ fglib/tests/test_rv.py
@@ -8,31 +8,62 @@ from .. import rv
 class TestDiscrete(unittest.TestCase):
 
     def setUp(self):
-        #TODO: ...
-        pass
+        self.d1 = rv.Discrete([0.6, 0.4], [0])
+        self.d2 = rv.Discrete([0.2, 0.8], [1])
+        self.d3 = rv.Discrete([[0.1, 0.2], [0.3, 0.4]], [0, 1])
 
     def test_equality(self):
-        #TODO: ...
-        pass
+        self.assertEqual(self.d1, self.d1)
+        self.assertNotEqual(self.d1, self.d2)
 
     def test_initialization(self):
-        #TODO: ...
-        pass
+        with self.assertRaises(rv.ParameterException):
+            rv.Discrete([[0.1, 0.2]], [0])
 
+    @unittest.skip("Test case is not implemented.")
     def test_addition(self):
         #TODO: ...
         pass
 
+    @unittest.skip("Test case is not implemented.")
     def test_subtraction(self):
         #TODO: ...
         pass
 
     def test_multiplication(self):
-        #TODO: ...
-        pass
+        tmp = np.array([0.36, 0.16])
+        npt.assert_almost_equal((self.d1 * self.d1).pmf, tmp / np.sum(tmp))
+        tmp = np.array([[0.06, 0.12], [0.12, 0.16]])
+        npt.assert_almost_equal((self.d1 * self.d3).pmf, tmp / np.sum(tmp))
+        tmp = np.array([[0.02, 0.16], [0.06, 0.32]])
+        npt.assert_almost_equal((self.d2 * self.d3).pmf, tmp / np.sum(tmp))
+        tmp = np.array([[0.01, 0.04], [0.09, 0.16]])
+        npt.assert_almost_equal((self.d3 * self.d3).pmf, tmp / np.sum(tmp))
 
     def test_unit_element(self):
-        #TODO: ...
+        ue = rv.Discrete([0.5, 0.5], [0])
+        self.assertEqual(self.d1 * ue, self.d1)
+        ue = rv.Discrete([[0.25, 0.25], [0.25, 0.25]], [0, 1])
+        self.assertEqual(self.d3 * ue, self.d3)
+
+    def test_argmax(self):
+        self.assertEqual(self.d1.argmax(), (0,))
+        self.assertEqual(self.d3.argmax(), (1, 1))
+        self.assertEqual(self.d3.argmax((0,)), (1,))
+
+    def test_max(self):
+        self.assertEqual(self.d1.max(), 0.6)
+        self.assertEqual(self.d3.max(), 0.4)
+        npt.assert_almost_equal(self.d3.max((0,)), 0.7)
+
+    def test_marginal(self):
+        tmp = np.array([0.3, 0.7])
+        npt.assert_almost_equal(self.d3.marginal((0,)).pmf, tmp)
+        tmp = np.array([0.4, 0.6])
+        npt.assert_almost_equal(self.d3.marginal((1,)).pmf, tmp)
+
+    @unittest.skip("Method is not implemented.")
+    def test_log(self):
         pass
 
 
@@ -80,7 +111,6 @@ class TestGaussian(unittest.TestCase):
     def test_unit_element(self):
         ue = rv.Gaussian([[0]], [[np.Inf]])
         self.assertEqual(self.g1 * ue, self.g1)
-
         ue = rv.Gaussian([[0], [0]], [[np.Inf, 0], [0, np.Inf]])
         self.assertEqual(self.g3 * ue, self.g3)
 
@@ -95,8 +125,8 @@ class TestGaussian(unittest.TestCase):
         self.assertEqual(self.g4.max(),
                          2 * np.pi * np.sqrt(np.linalg.det(self.g4.cov)))
 
-    def test_int(self):
-        self.assertEqual(self.g4.int([0]), self.g1)
+    def test_marginal(self):
+        self.assertEqual(self.g4.marginal([0]), self.g1)
 
     @unittest.skip("Method is not implemented.")
     def test_log(self):
