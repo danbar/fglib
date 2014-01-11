73a06312d24e9065049900b6b85ce8dac77b7b81
Daniel Bartel <dan.bar@gmx.at>
2014-01-11 16:52:48 +0100

    Add file for unit tests of factor graph objects.

---
+++ fglib/tests/test_object.py
@@ -0,0 +1,17 @@
+import unittest
+
+from .. import object
+
+
+class TestNode(unittest.TestCase):
+
+    pass
+
+
+class TestEdge(unittest.TestCase):
+
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
