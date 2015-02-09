f8e8ec37e461b08924bc0d6a08bb69cee826b4fa
Daniel Bartel <danbar@users.noreply.github.com>
2015-02-09 20:53:28 +0100

    bugfix

--- fglib/tests/test_graphs.py
+++ fglib/tests/test_graphs.py
@@ -14,15 +14,15 @@ class TestFactorGraph(unittest.TestCase):
         g.add_nodes_from(['fa', 'fb', 'fc'], bipartite=1)
         g.add_edges_from([('x1', 'fa'), ('fa', 'x2'),
                           ('x2', 'fb'), ('fb', 'x3'),
-                          ('x2', 'fc'), ('fc', 'x2')])
+                          ('x2', 'fc'), ('fc', 'x4')])
         bottom_nodes, top_nodes = bipartite.sets(g)
 
         fg = graphs.convert_graph_to_factor_graph(g, nodes.VNode, nodes.FNode)
         vn = {str(n) for n in fg.get_vnodes()}
         fn = {str(n) for n in fg.get_fnodes()}
 
-        self.assertSetEqual(top_nodes, vn)
-        self.assertSetEqual(bottom_nodes, fn)
+        self.assertSetEqual(bottom_nodes, vn)
+        self.assertSetEqual(top_nodes, fn)
 
 
 if __name__ == "__main__":
