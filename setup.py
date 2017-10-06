59f886c7d6d0837a719d798ce0401cb73892db0e
Daniel Bartel <danbar@users.noreply.github.com>
2017-10-06 23:01:52 +0200

    setup update

--- setup.py
+++ setup.py
@@ -5,11 +5,21 @@ import fglib
 setup(
     name="fglib",
     version=fglib.__version__,
-    url="https://github.com/danbar/fglib/",
-    license="MIT License",
-    author="Daniel Bartel",
-    install_requires=["numpy>=1.9"],
-    description="factor graph library",
     packages=find_packages(),
+    scripts=[],
+
+    # Dependencies
+    install_requires=["networkx>=2.0",
+                      "numpy>=1.8",
+                      "matplotlib>=1.3"],
+
+    # Metadata
+    author="Daniel Bartel",
+    description="The factor graph library (fglib) is a Python 3 package to simulate message passing on factor graphs.",
+    license="MIT License",
+    keywords="factor graph message passing",
+    url="https://github.com/danbar/fglib/",
+
+    # Miscellaneous
     test_suite="nose.collector"
 )
