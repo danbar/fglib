c7eaff440f040a745b36c06fb5392e1950f2ce7a
Daniel Bartel <danbar@users.noreply.github.com>
2017-10-19 21:31:02 +0200

    fix unittest issue regarding bipartite sets

--- fglib/tests/test_graphs.py
+++ fglib/tests/test_graphs.py
@@ -9,13 +9,16 @@ from .. import graphs, nodes
 class TestFactorGraph(unittest.TestCase):
 
     def test_convert_graph_to_factor_garph(self):
+        variable_nodes = ['x1', 'x2', 'x3', 'x4']
+        factor_nodes = ['fa', 'fb', 'fc']
+
         g = nx.Graph()
-        g.add_nodes_from(['x1', 'x2', 'x3', 'x4'], bipartite=0)
-        g.add_nodes_from(['fa', 'fb', 'fc'], bipartite=1)
+        g.add_nodes_from(variable_nodes, bipartite=0)
+        g.add_nodes_from(factor_nodes, bipartite=1)
         g.add_edges_from([('x1', 'fa'), ('fa', 'x2'),
                           ('x2', 'fb'), ('fb', 'x3'),
                           ('x2', 'fc'), ('fc', 'x4')])
-        bottom_nodes, top_nodes = bipartite.sets(g)
+        bottom_nodes, top_nodes = bipartite.sets(g, variable_nodes)
 
         fg = graphs.convert_graph_to_factor_graph(g, nodes.VNode, nodes.FNode)
         vn = {str(n) for n in fg.get_vnodes()}
