a0ed76643744a2d8d952496edb7e7d7e83daffa1
Daniel Bartel <danbar@users.noreply.github.com>
2017-11-16 21:41:03 +0100

    add setup arguments

--- setup.py
+++ setup.py
@@ -12,19 +12,30 @@ setup(
     scripts=[],
 
     # Dependencies
+    python_requires='>=3.4',
     install_requires=["networkx>=2.0",
                       "numpy>=1.12",
                       "matplotlib>=2.0"],
 
     # Metadata
     author="Daniel Bartel",
-    author_email = 'dan.bar@gmx.at',
+    author_email='dan.bar@gmx.at',
     description="factor graph library",
     long_description=long_description,
     license="MIT License",
     keywords="factor graph message passing",
     url="https://github.com/danbar/fglib/",
-    download_url = 'https://github.com/danbar/fglib/archive/v0.2.1.tar.gz',
+    download_url='https://github.com/danbar/fglib/archive/v0.2.1.tar.gz',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 
     # Miscellaneous
     test_suite="nose.collector"
